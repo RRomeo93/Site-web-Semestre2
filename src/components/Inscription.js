e9ed09653d925b13a922d6dfab4e7f18873efd91
Adem-y78 <adem.yigit78300@gmail.com>
2024-02-25 13:14:26 +0100

    Page de connexion et inscription terminé

--- src/components/Inscription.js
+++ src/components/Inscription.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React,{ useState }  from 'react';
 import '../css/style_inscription.css';
 function Inscription() {
-  return <>
-  <div class="container_inscription">
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    ENDROIT POUR LA PAGE inscription
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Logique de gestion de la soumission du formulaire d'inscription
+  };
+
+  return (
+    <div className="container_inscription">
+    <h2>Get’s started.</h2>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={handleNameChange}
+        placeholder="Name"
+        required
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={handleEmailChange}
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+        placeholder="Password"
+        required
+      />
+      <input
+        type="password"
+        value={confirmPassword}
+        onChange={handleConfirmPasswordChange}
+        placeholder="Confirm Password"
+        required
+      />
+      <button type="submit" className="inscription-button">Sign Up</button>
+    </form>
   </div>
-  </>;
+  );
 }
 
 export default Inscription;
